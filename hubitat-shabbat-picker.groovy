08e755f2c54f6df70f2be6f0b385c1270f6a3b10
dds82 <47287470+dds82@users.noreply.github.com>
2021-04-15 13:27:44 -0400

    Create hubitat-shabbat-picker.groovy

---
+++ hubitat-shabbat-picker.groovy
@@ -0,0 +1,172 @@
+import groovy.transform.Field
+import java.util.Date
+import java.util.Calendar
+import java.text.SimpleDateFormat
+
+metadata {
+ 	definition (name: "Shabbat Time Selector", namespace: "smartdevices", author: "Daniel Segall") {
+        capability "Actuator"
+        capability "PushableButton"
+        attribute "regularTime","number"
+        attribute "earlyTime","number"
+        attribute "plagTime","number"
+        attribute "activeTime", "number"
+        attribute "times", "string"
+        attribute "activeType", "enum", ["Regular", "Plag", "Early"]
+        command "setRegularTime", ["long"]
+        command "regular"
+        command "plag"
+        command "early"
+     }
+ }
+
+ def parse() {
+ }
+
+def installed() {
+    initialize()
+    plag()
+}
+ 
+ def initialize() {
+    Date regular = new Date(location.sunset.getTime() - (18 * 60000))
+     setRegularTime(regular)
+ }
+
+def setRegularTime(long date) {
+    sendEvent("name":"regularTime", "value":date)
+    def activeType = device.currentValue("activeType")
+    if (activeType == null)
+        activeType = "Plag"
+    
+    updateActiveTime(activeType, regularTimeOnCalendar(date))
+}
+
+def plag() {
+    updateActiveTime("Plag")
+}
+
+def regular() {
+    updateActiveTime("Regular")
+}
+
+def early() {
+    updateActiveTime("Early")
+}
+ 
+ def updated() {
+     initialize()
+ }
+
+def updateActiveTime(type) {
+    Calendar regular = regularTimeOnCalendar()
+    updateActiveTime(type, regular)
+}
+
+def updateActiveTime(type, regular) {
+    int time = (regular.get(Calendar.HOUR_OF_DAY) * 100) + regular.get(Calendar.MINUTE)
+    def regularTime = regular.getTimeInMillis()
+    
+    // plag
+    regular.add(Calendar.HOUR_OF_DAY, -1)
+    def plagTime = regular.getTimeInMillis()
+    
+    // 7pm
+    regular.add(Calendar.HOUR_OF_DAY, 1)
+    regular.set(Calendar.HOUR_OF_DAY, 19)
+    regular.set(Calendar.MINUTE, 0)
+    def earlyTime = regular.getTimeInMillis()
+    
+    def activeTime = null
+    
+    boolean earlyOption = time >= 1850
+    if (earlyOption) {
+        switch (type) {
+            case "Regular":
+                activeTime = regularTime
+                break
+        
+            case "Plag":
+                activeTime = plagTime
+                break
+        
+            case "Early":
+                activeTime = earlyTime
+                break
+        }
+    }
+    else
+        activeTime = regularTime
+    
+    sendEvent("name":"activeType", "value":type)
+    sendEvent("name":"activeTime", "value":activeTime)
+    sendEvent("name":"earlyTime", "value":earlyTime)
+    sendEvent("name":"plagTime", "value":plagTime)
+    updateTimes(earlyOption, earlyTime, plagTime, regularTime, type)
+}
+
+def updateTimes(boolean earlyOption, long earlyTime, long plagTime, long regularTime, String activeType) {
+    def times
+    
+    SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd @ h:mm a")
+    final String headerBegin = "<font color=\"red\">"
+    final String headerEnd = "</font>"
+    
+    if (earlyOption) {
+        String text = ""
+        if (activeType == "Plag")
+            text += headerBegin
+        text += sdf.format(new Date(plagTime))
+        if (activeType == "Plag")
+            text += headerEnd
+        text += "<br>"
+        
+        if (activeType == "Early")
+            text += headerBegin
+        text += sdf.format(new Date(earlyTime))
+        if (activeType == "Early")
+            text += headerEnd
+        
+        text += "<br>"
+        
+        if (activeType == "Regular")
+            text += headerBegin
+        text += sdf.format(new Date(regularTime))
+        if (activeType == "Regular")
+            text += headerEnd
+        
+        times = text
+    }
+    else {
+        times = sdf.format(new Date(regularTime))
+    }
+    
+    sendEvent("name":"times", "value": times)
+}
+
+Calendar regularTimeOnCalendar() {
+    return regularTimeOnCalendar(device.currentValue("regularTime"))
+}
+
+Calendar regularTimeOnCalendar(currTime) {
+    Calendar cal = Calendar.getInstance()
+    cal.setTimeInMillis(currTime.longValue())
+    return cal
+}
+
+def push(num) {
+    switch (num.toInteger()) {
+        case 0:
+            plag()
+            break
+        
+        case 1:
+            early()
+            break
+        
+        case 2:
+            regular()
+            break
+    }
+}
+    
