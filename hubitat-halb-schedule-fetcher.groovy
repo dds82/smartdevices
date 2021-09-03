aee7261b8f16baa94a9fa469be28f99f476276ba
dds82 <47287470+dds82@users.noreply.github.com>
2021-09-03 08:26:36 -0400

    Update hubitat-halb-schedule-fetcher.groovy
    set minute to 0

--- hubitat-halb-schedule-fetcher.groovy
+++ hubitat-halb-schedule-fetcher.groovy
@@ -444,6 +444,7 @@ def updateStatusText(String forceDate=null) {
         cal.set(Calendar.SECOND, 0)
         cal.set(Calendar.MILLISECOND, 0)
         cal.set(Calendar.HOUR_OF_DAY, 0)
+        cal.set(Calendar.MINUTE, 0)
         cal.set(Calendar.SECOND, 0)
     }
     else {
